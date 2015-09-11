aa79d3fc4f8676d2cbb50268bd66247b29f64efe
Patrick Halton <phalton04@fullsail.edu>
2015-09-11 19:29:32 -0400

    link for flight training

--- js/main.js
+++ js/main.js
@@ -2,21 +2,45 @@
  * Created by patrickhalton on 8/31/15.
  */
 
-var smsApp = angular.module('smsApp', ["firebase"]);
+angular.module('smsApp', ['ngRoute','firebase','ui.bootstrap']).config(["$routeProvider",function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'www/views/signLog.html',
+            controller: 'formController'
 
-smsApp.controller('mainController',["$scope","$firebaseArray", function ($scope,$firebaseArray) {
+        })
+        .when('/chat', {
+            templateUrl: 'www/views/chat.html',
+            controller: 'mainController',
+            authRequired: true
+        })
+
+        .when('/userChat/:id', {
+            templateUrl: 'www/views/userChat.html',
+            controller: 'chaterController',
+            authRequired: true
+        })
+
+
+
+}])
+
+.controller('mainController',["$scope","$firebaseArray","$firebaseAuth","$location", function ($scope,$firebaseArray,$firebaseAuth,$location) {
 
     var ref  = new Firebase("https://smrproj.firebaseio.com/comments");
-    var userDoc = new Firebase("https://smrproj.firebaseio.com/users");
+    var ref2  = new Firebase("https://smrproj.firebaseio.com/chat");
 
-    $scope.comments = $firebaseArray(ref);
+        $scope.makeChat = $firebaseArray(ref2);
+        $scope.authObj = $firebaseAuth(ref);
+        $scope.comments = $firebaseArray(ref);
 
-    $scope.username = "patient" + Math.floor(Math.random() * 10);
+
+        $scope.username = "patient" + Math.floor(Math.random()*11);
 
     $scope.addComment = function(e){
         if(e){
             $scope.comments.$add({
-                from: $scope.username,
+                from: $scope.authData.password.email,
                 body: $scope.newComment
             });
 
@@ -25,23 +49,124 @@ smsApp.controller('mainController',["$scope","$firebaseArray", function ($scope,
         }
     };
 
-    $scope.name = "";
-    $scope.pass = "";
+        $scope.authObj.$onAuth(function(authData) {
+            if (authData) {
+                console.log(authData);
+                console.log("im in:", authData.uid);
+                $scope.authData = authData;
+            } else {
+                $location.path("/");
+                console.log("Logged out");
+            }
+        });
 
-    $scope.addForm = function(e){
-    if(e){
+        $scope.addChat = function(){
 
-        $scope.users.$add({
-            user :$scope.name,
-            pass :$scope.pass
-        })
+            $scope.makeChat.$add({
+                chat: $scope.newChat,
+                user: $scope.authData.password.email
+            })
+                .then(function(ref) {
+                    var id = ref.key();
+                    console.log("added record with id " + id);
+                    $scope.makeChat.$indexFor(id); // returns location in the array
+                    $location.path("/userChat/"+ id);
+                });
+
+        };
+
+}])
+
+.controller('formController',["$scope","$firebaseArray","$firebaseAuth","$location", function ($scope,$firebaseArray,$firebaseAuth,$location) {
+
+    var userDoc = new Firebase("https://smrproj.firebaseio.com");
+
+    $scope.authObj = $firebaseAuth(userDoc);
+    $scope.signIn = false;
+
+    $scope.addForm = function(){
+
+        $scope.authObj.$createUser({
+
+            email: $scope.newUser.email,
+            name: $scope.login.username,
+            password: $scope.newUser.pass
+
+        }).then(function(userData) {
+                console.log("User " + userData.uid + " created successfully!");
+
+                return $scope.authObj.$authWithPassword({
+
+                    email: $scope.newUser.email,
+                    name: $scope.login.username,
+                    password: $scope.newUser.pass
+
+                });
+            }).then(function(authData) {
+                console.log("Logged in as:", authData.uid);
+                $location.path("/chat")
+            }).catch(function(error) {
+                $scope.errMess2 = "Error: Email already in use"
+            });
+    };
+
+    $scope.login = function(){
+
+        $scope.authObj.$authWithPassword({
+
+            email: $scope.login.email,
+            password: $scope.login.password
+
+        }).then(function(authData) {
+                $location.path("/chat");
+                console.log("Logged in as:", authData.uid);
+            }).catch(function(error) {
+
+                $scope.errMess = "Invalid Login";
+
+                console.error("Authentication failed:", error);
+            });
 
     }
 
 
+}])
 
-    }
+ .controller('chaterController',["$scope","$firebaseArray","$firebaseAuth","$location","$routeParams", function ($scope,$firebaseArray,$firebaseAuth,$location,$routeParams) {
+
+        var ref3  = new Firebase("https://smrproj.firebaseio.com/chat/" + $routeParams.id);
+
+        $scope.authObj = $firebaseAuth(ref3);
+        $scope.privChat = $firebaseArray(ref3);
+
+        $scope.chatId = $routeParams.id;
 
 
+        $scope.addToChat = function(e){
+            if(e){
+                $scope.privChat.$add({
+                    from: $scope.authData.password.email,
+                    body: $scope.newComment
+                });
 
-}]);
+                $scope.newComment = "";
+
+            }
+        };
+
+        $scope.authObj.$onAuth(function(authData) {
+            if (authData) {
+        console.log(authData);
+                console.log("im in:", authData.uid);
+                $scope.authData = authData;
+            } else {
+                $location.path("/");
+                console.log("Logged out");
+            }
+        });
+
+console.log($scope.privChat);
+
+    }]);
+
+
